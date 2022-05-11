0c6a8f1100a0c7cae9d7aa2bd8180767f5f765cb
HybridAvenger69 <hybridavenger69@yahoo.com>
2022-05-11 17:30:12 -0600

    1.18.2 Upload
    initial upload of 1.18.2 HybridLib

---
+++ 1.18.2/src/main/java/com/hybridavenger69/hlib/main/ItemRegistry.java
@@ -0,0 +1,34 @@
+package com.hybridavenger69.hlib.main;
+
+import com.hybridavenger69.hlib.HybridLib;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemRegistry {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, HybridLib.MOD_ID);
+
+    public static final RegistryObject<Item> NETHER_INGOT = ITEMS.register("nether_ingot",
+            () -> new Item(new Item.Properties().tab(MtGroup.MT_GROUP)));
+
+    public static final RegistryObject<Item> TECH_INGOT = ITEMS.register("tech_ingot",
+            () -> new Item(new Item.Properties().tab(MtGroup.MT_GROUP)));
+
+    public static final RegistryObject<Item> RAW_TECH = ITEMS.register("raw_tech",
+            () -> new Item(new Item.Properties().tab(MtGroup.MT_GROUP)));
+
+    public static final RegistryObject<Item> EUCLASE_GEM = ITEMS.register("euclase_gem",
+            () -> new Item (new Item.Properties().tab(MtGroup.MT_GROUP)));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
